9662699fa54ad9076252015e0b6d0232fe978375
NathaliaMinoque <75228926+NathaliaMinoque@users.noreply.github.com>
2024-01-05 09:29:34 +0700

    re-init

---
+++ Certification/CertificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CertificationApp.swift
+//  Certification
+//
+//  Created by Nathalia Minoque Kusuma Salma Rasyid Jr. on 05/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CertificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
